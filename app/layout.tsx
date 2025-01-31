89cd32bc44b2b54472ece3195512f5bdcade49d6
Luis Eduardo <luiseperezp.10@gmail.com>
2025-01-30 20:19:40 -0500

    feat: change the Metadata import to a more consistent style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-﻿import type { Metadata } from "next";
+﻿import { Metadata } from 'next';
 import { GoogleSans } from './fonts';
 import "./globals.scss";
 import { ThemeProvider } from './context/ThemeContext';
